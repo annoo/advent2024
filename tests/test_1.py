52ae16b86ab7102d170e5cac717fff8dfd95b298
Ann <ann.peeters.email@gmail.com>
2024-12-07 22:50:04 +0100

    solve part2 of day 1

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,18 +1,19 @@
 import pytest
-from day1.solution import solve_part1  #, solve_part2
+from day1.solution import solve_part1, solve_part2
 
+input = "day1/test_intput.txt"
 
 @pytest.mark.parametrize(
     "part, input_data, expected_output",
     [
-        (1, "day1/test_intput.txt", 11),
-        #(2, "input_1", 281),
+        (1, input, 11),
+        (2, input, 31),
     ],
 )
 def test_solve(part, input_data, expected_output):
     if part == 1:
         result = solve_part1(input_data)
-    # else:
-    #     result = solve_part2(input_data)
+    else:
+        result = solve_part2(input_data)
     print(f"\n\n{result=}")
     assert result == expected_output
